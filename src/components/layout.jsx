d3a5f48e098cc1b923f2e332e70c3c1d3d60bfb8
jmargielewski <kuba.margielewski@gmail.com>
2019-05-09 21:47:20 +0100

    gatsby initial commit

---
+++ src/components/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './bootstrap.min.css';
+import './layout.css';
+
+const Layout = ({ children }) => <>{children}</>;
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
