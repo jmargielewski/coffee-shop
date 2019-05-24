c19a42ca968b6477a564603d455c917f5daca882
jmargielewski <kuba.margielewski@gmail.com>
2019-05-24 21:53:58 +0100

    about page & connection to Contentful

---
+++ src/pages/about.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import BackgroundSection from '../components/BackgroundSection';
+import SEO from '../components/seo';
+import Info from '../components/Home';
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="about us"
+      styleClass="default-background"
+    />
+    <Info />
+  </Layout>
+);
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+export default AboutPage;
