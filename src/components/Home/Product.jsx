c60a9b9b3e8a312722184786971e206f9b9cc8e7
jmargielewski <kuba.margielewski@gmail.com>
2019-05-26 10:05:20 +0100

    add product component

--- src/components/Home/Product.jsx
+++ src/components/Home/Product.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import Img from 'gatsby-image';
 
-const Product = () => <div>Hello product</div>;
+const Product = ({ product }) => (
+  <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-3">
+    <div className="card" style={{ minHeight: '100%' }}>
+      <Img fluid={product.image.fluid} className="card-img-top" />
+      <div className="card-body text-center">
+        <h6>{product.title}</h6>
+        <h6>{product.proce}</h6>
+        <button type="button" className="btn btn-yellow mt-3 text-capitalize">
+          add to cart
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default Product;
