abe77868bcdc17804c8c316cddefeb9fc13b1c23
jmargielewski <kuba.margielewski@gmail.com>
2019-05-26 18:23:17 +0100

    snipcart setup

--- src/components/Home/Product.jsx
+++ src/components/Home/Product.jsx
@@ -8,7 +8,15 @@ const Product = ({ product }) => (
       <div className="card-body text-center">
         <h6>{product.title}</h6>
         <h6>{product.proce}</h6>
-        <button type="button" className="btn btn-yellow mt-3 text-capitalize">
+        <button
+          type="button"
+          className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
+          data-item-id={product.id}
+          data-item-name={product.title}
+          data-item-price={product.price}
+          data-item-image={product.image.fluid.src}
+          data-item-url="https://coffee-shop-page.netlify.com/"
+        >
           add to cart
         </button>
       </div>
